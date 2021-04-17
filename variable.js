9d72bb239b92b499557e04a323f30524c98e5fce
fstufop <68706037+fstufop@users.noreply.github.com>
2021-04-17 11:46:31 -0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,48 @@
+//declare variables and assign string, boolean, undefined and null data types
+var name = 'Filipe'
+var question = true
+var what
+var nothing = null
+console.log(name,question,what,nothing)
+
+//Declare four variables without assigning values
+var empty1
+var empty2
+var empty3
+var empty4
+
+//Declare four variables with assigned values
+var full1
+var full2
+var full3
+var full4
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var firstName1 = "Filipe"
+var lastName1 = "Teodoro"
+var martialStatus1 ="Married"
+var country1 = "Brazil"
+var age1 = "32"
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+var firstName2 = "Isadora",
+    lastName2 = "Sampaio",
+    martialStatus2 ="Married",
+    country2 = "Brazil",
+    age2 = "31"
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+var myAge = 32
+var yourAge = 31
+    console.log(myAge,yourAge)
+var fullname = firstName1 + " " + lastName1
+console.log(`${myAge} ${yourAge}`)
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
+
+console.log(paragraph)
