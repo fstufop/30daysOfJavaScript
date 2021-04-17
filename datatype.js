4e3592cb7431fd7ecd1f2d0c133adda9d2d25df6
fstufop <fstufop@gmail.com>
2021-04-17 11:47:31 -0300

    data

---
+++ datatype.js
@@ -0,0 +1,6 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+var name = 'Filipe'
+var question = true
+var what
+var nothing = null
+console.log(typeof(name),typeof(question),typeof(what),typeof(nothing))
